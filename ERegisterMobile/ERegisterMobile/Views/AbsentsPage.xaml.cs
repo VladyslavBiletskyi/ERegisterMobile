4bf13633151763b47507f2005fc5b1f40b571c30
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2017-05-22 22:06:16 +0300

    Releazed absents tab

--- ERegisterMobile/ERegisterMobile/Views/AbsentsPage.xaml.cs
+++ ERegisterMobile/ERegisterMobile/Views/AbsentsPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ERegisterMobile.Models;
+using ERegisterMobile.Util;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,69 @@ namespace ERegisterMobile.Views
         public AbsentsPage()
         {
             InitializeComponent();
+            HttpClientEngine.AccessToken = Application.Current.Properties["token"]?.ToString();
+            List<LessonViewModel> lessons;
+            try
+            {
+                lessons =
+                    (List<LessonViewModel>) HttpClientEngine.Get("api/Lessons/Absents", typeof(List<LessonViewModel>));
+                FillElements(lessons);
+            }
+            catch
+            {
+                DisplayAlert("Error!", "Error while filling absents", "Ok");
+            }
+        }
+
+        private void FillElements(List<LessonViewModel> lessons)
+        {
+            Label header = new Label
+            {
+                Text = "Your absents",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+            int i = 1;
+            ListView listView = new ListView
+            {
+                HasUnevenRows = true,
+                ItemsSource = lessons,
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    Label elementNumberLabel = new Label { Text = "Absent №"+i++, HorizontalOptions = LayoutOptions.Center };
+                    Label subjectLabel = new Label {Text = "Subject name"};
+                    Label subjectName = new Label { TextColor=Color.Black };
+                    subjectName.SetBinding(Label.TextProperty, "Subject");
+                    Label dateTimeLabel = new Label { Text = "Begining date and time of the lesson" };
+                    Label beginingDateTime = new Label { TextColor = Color.Black };
+                    beginingDateTime.SetBinding(Label.TextProperty, "BeginigDateTime");
+                    Label numberLabel = new Label { Text = "Number of present" };
+                    Label numberOfPresent = new Label { TextColor = Color.Black };
+                    numberOfPresent.SetBinding(Label.TextProperty, "NumberOfPresent");
+                    Label averrageMarkLabel = new Label { Text = "Average mark on lesson" };
+                    Label averageMark = new Label { TextColor = Color.Black };
+                    averageMark.SetBinding(Label.TextProperty, "AverageMark");
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(0, 5),
+                            Orientation = StackOrientation.Vertical,
+                            Children = {
+                                elementNumberLabel,
+                                subjectLabel,
+                                subjectName,
+                                dateTimeLabel,
+                                beginingDateTime,
+                                numberLabel,
+                                numberOfPresent,
+                                averrageMarkLabel,
+                                averageMark }
+                        }
+                    };
+                })
+            };
+            Content = new StackLayout { Children = { header, listView } };
         }
     }
 }
--- ERegisterMobile/ERegisterMobile/Views/ProfilePage.xaml.cs
+++ ERegisterMobile/ERegisterMobile/Views/ProfilePage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web.ModelBinding;
+using ERegisterMobile.Models;
+using ERegisterMobile.Util;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,9 +17,46 @@ namespace ERegisterMobile.Views
         public ProfilePage()
         {
             InitializeComponent();
-            FirstNameLabel.Text = Application.Current.Properties["UserName"]?.ToString().Split(' ')[0];
-            LastNameLabel.Text = Application.Current.Properties["UserName"]?.ToString().Split(' ')[1];
-            EmailLabel.Text = "vladyslav.biletskyi@nure.ua";
+            HttpClientEngine.AccessToken = Application.Current.Properties["token"]?.ToString();
+            var model = (UserDetailsViewModel)HttpClientEngine.Get("api/Account/GetUserInfo", typeof(UserDetailsViewModel));
+            FirstNameLabel.Text = model.FirstName;
+            LastNameLabel.Text = model.LastName;
+            GroupLabel.Text = model.Group;
+        }
+        public async void ChangePassword()
+        {
+            HttpClientEngine.AccessToken = Application.Current.Properties["token"]?.ToString();
+            if (String.IsNullOrEmpty(OldPassword.Text)||
+                String.IsNullOrEmpty(NewPassword.Text) ||
+                String.IsNullOrEmpty(ConfirmPassword.Text))
+            {
+                DisplayAlert("Error!", "Fields must be filled", "Ok");
+                return;
+            }
+            if (NewPassword.Text != ConfirmPassword.Text)
+            {
+                DisplayAlert("Error!", "Password and confirmition must be the same", "Ok");
+            }
+            try
+            {
+                ChangePasswordModel model = new ChangePasswordModel
+                {
+                    OldPassword = OldPassword.Text,
+                    NewPassword = NewPassword.Text,
+                    ConfirmPassword = ConfirmPassword.Text
+                };
+                await HttpClientEngine.Post("api/Account/ChangePassword", model);
+                DisplayAlert("Success!", "Password completely changed", "Ok");
+            }
+            catch
+            {
+                DisplayAlert("Error!", "Error while changinp password. Check fields and try again", "Ok");
+            }
+        }
+
+        private void ChangePasswordButton_Clicked(object sender, EventArgs e)
+        {
+            ChangePassword();
         }
     }
 }
